6b9b2e86b903378542d30f136f2cc4bf186509f3
DXHASE <danielchas886@gmail.com>
2022-08-24 23:32:07 -0500

    Added to README

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Weather-Dashboard
+## Purpose
+* A website that will allow me to view the forecast of a specific city and the current climate of that city once I input it in the search bar. 
+
+## Built With
+* HTML
+* CSS
+* JavaScript
+
+## Website
+ https://dxhase.github.io/Weather-Dashboard/
+
+## Creator
+Daniel Chase 
+August 2022
+
+![Image of Portfolio](./images/Weather-Dashboard.png)
--- js/script.js
+++ js/script.js
@@ -15,30 +15,29 @@ var getCoordinates = function(cityName){
 var getWeatherInfo = function(data){
     var lat = data[0].lat;
     var long = data[0].lon;
-    var apiUrl="https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long + "&appid=b6111499107ea4a9ba60b48a4c1c1ca6";
+    var apiUrl="https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + long + "&appid=b6111499107ea4a9ba60b48a4c1c1ca6";
     console.log(lat,long);
     fetch(apiUrl)
     .then(function(response){
         return response.json();
     })
     .then(function(data){
-        var weather = data.current;
-        console.log(data);
-    });
+        console.log(data.list);
+    })
 }
+var displayWeatherInfo = function(){
+ 
+}
+displayWeatherInfo();
 function formSubmitHandler(event) {
     event.preventDefault();
     var city = userCity.value.trim();
     if(city){
         getCoordinates(city);
         userCity.value= "";
-        console.log(city);
     }else{
         alert(" Enter a city name ");
     }
 }
-var displayWeatherInfo = function(){
-  
-}
-displayWeatherInfo();
+
 searchForm.addEventListener("submit",formSubmitHandler);
